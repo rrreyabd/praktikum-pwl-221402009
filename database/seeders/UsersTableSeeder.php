f141e66322b2f7eec2ddb37a8406f4811892d7fc
Raihan Abdillah <raihanabdillahlubis@gmail.com>
2023-04-19 19:20:53 +0700

    pertemuan 4

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory;
+use DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [];
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $users [] = [
+                'name' => $faker->name,
+                'user_role' => $faker->randomElement($array = array('Admin', 'Customer')),
+                'email' => $faker-> email,
+                'email_verified_at' => $faker->dateTime(),
+                'password' => $faker -> password 
+            ];
+        }
+
+        DB::table("users")->insert($users);
+    }
+}
